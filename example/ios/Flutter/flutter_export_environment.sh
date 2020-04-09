c594e924b19ed366da347c08b14b0efe954a3b40
iamsmiling <1412830255@qq.com>
2020-04-09 21:57:04 +0800

    init

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/iamsmiling/sdk/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/iamsmiling/Library/Containers/com.tencent.xinWeChat/Data/Library/Application Support/com.tencent.xinWeChat/2.0b4.0.9/1af7d305df0848d8e712758e97464c67/Message/MessageTemp/0154a6e1d009c25c5d144ced440da2c6/File/zhongya_android-develop/webview_flutter/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/iamsmiling/sdk/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.3"
+export "FLUTTER_BUILD_NUMBER=1.0.3"
